b94827793ca9163bab005e3661765adac30f8fe5
Kushagra5678 <kushu.7feb@gmail.com>
2020-09-26 22:11:12 +0530

    Master commit

---
+++ userRegistrationStart/src/com/practice/userRegistrationStart/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.practice.userRegistrationStart;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to user Registration System");
+	}
+
+}
