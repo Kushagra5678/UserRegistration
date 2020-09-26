206524bce61c5d5ed41072ded24f772b173f3c5b
Kushagra5678 <kushu.7feb@gmail.com>
2020-09-26 22:18:02 +0530

    UC3 commit

---
+++ userRegistrationUC3/src/com/practice/UC3/UserRegistration.java
@@ -0,0 +1,22 @@
+package com.practice.UC3;
+
+import java.util.*;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Enter valid email id: ");
+		Scanner sc = new Scanner(System.in);
+		String email=sc.nextLine();
+		//boolean x = email.matches("[a-zA-Z0-9+_-]+(.[a-zA-Z0-9+_-]*)@");
+		//boolean x=email.matches("([^\\s])+");
+		//boolean x=email.matches("[.][\\w]{1,}");
+		//boolean x=email.matches("[.][\\w]{3,}.?");ignore
+		//boolean x=email.matches("[.][a-zA-Z]{2}.?");ignore
+		//boolean x=email.matches("([.][\\\\w]{3,}.?|[.][a-zA-Z]{2}.?)?");
+		boolean x = email.matches("[a-zA-Z0-9+_-]+(.[a-zA-Z0-9+_-]*)@([^\\s])+[.][\\w]{1,}([.][\\w]{3,}.?|[.][a-zA-Z]{2}.?)?");
+		System.out.println(x);
+	}
+
+}
